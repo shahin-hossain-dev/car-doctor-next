b0e8a38c440cdc18367f2a2ef994cbdd96061b18
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-08-01 01:05:07 +0600

    80-2 add:  daisy ui custom theme

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Car Doctor Next</h2>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <button className="btn btn-primary text-white">Button</button>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,4 +15,19 @@ module.exports = {
     },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        carDoctorTheme: {
+          primary: "#FF3811",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 };
